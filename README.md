d4618c119e545c43201d60bc2cbd0c5ef9ad2e73
Jason Watmore <cornflourblue@hotmail.com>
2014-06-23 20:09:31 +1000

    initial commit

--- README.md
+++ README.md
@@ -2,3 +2,28 @@ angular-reverse-geocode
 =======================
 
 AngularJS reverse geocoding directive
+
+
+###Demo
+
+To see a demo and further details go to http://jasonwatmore.com/post/2014/02/15/AngularJS-Reverse-Geocoding-Directive.aspx
+
+###Installation
+
+Install using bower: `bower install angular-reverse-geocode`
+
+Alternatively download the code and include the angular-reverse-geocode.js file in your page.
+
+Add the 'angularReverseGeocode' directive as a dependency of your AngularJS application:
+
+```javascript
+angular.module('myApp', ['angularReverseGeocode']);
+```
+
+###Usage
+
+To use add a reverse-geocode tag to your page with attributes containing lat and long coordinates, e.g:
+
+```html
+<reverse-geocode lat="40.730885" lng="-73.997383"></reverse-geocode>
+```
---
+++ angular-reverse-geocode.js
@@ -0,0 +1,30 @@
+﻿/**
+ * AngularJS reverse geocoding directive
+ * @author Jason Watmore <jason@pointblankdevelopment.com.au> (http://jasonwatmore.com)
+ * @version 1.0.0
+ */
+(function () {
+    angular.module('angularReverseGeocode', [])
+    .directive('reverseGeocode', function () {
+        return {
+            restrict: 'E',
+            template: '<div></div>',
+            link: function (scope, element, attrs) {
+                var geocoder = new google.maps.Geocoder();
+                var latlng = new google.maps.LatLng(attrs.lat, attrs.lng);
+                geocoder.geocode({ 'latLng': latlng }, function (results, status) {
+                    if (status == google.maps.GeocoderStatus.OK) {
+                        if (results[1]) {
+                            element.text(results[1].formatted_address);
+                        } else {
+                            element.text('Location not found');
+                        }
+                    } else {
+                        element.text('Geocoder failed due to: ' + status);
+                    }
+                });
+            },
+            replace: true
+        }
+    });
+})();
